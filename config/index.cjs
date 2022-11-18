22ad3d9b4a1dc630e0db64c1b33507759dcaf099
1zhiyu <112168605+1zhiyu@users.noreply.github.com>
2022-11-18 20:07:59 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '宝贝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oyP4T5za0w5v-xjf60lU9HkRIQ2s',
+      id: 'oyP4T52FYEhUhrfOTwQ_IBFeXqUg',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: '0i0CMm6mYpaI2JvT3iJRhjuIthGM3knbHcYJQo2xaC4',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
@@ -48,13 +48,13 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'oyP4T5za0w5v-xjf60lU9HkRIQ2s',
+  CALLBACK_TEMPLATE_ID: 'oyP4T52FYEhUhrfOTwQ_IBFeXqUg',
 
   CALLBACK_USERS: [
     {
       name: '宝贝',
       // 使用微信测试号：自己的微信id，扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oyP4T5za0w5v-xjf60lU9HkRIQ2s',
+      id: 'oyP4T52FYEhUhrfOTwQ_IBFeXqUg',
     }
   ],
 
